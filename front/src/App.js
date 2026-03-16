550d01e99497a8b77149f84b0e371a80fb1e6729
Tioui-Badr <b.tioui@esisa.ac.ma>
2026-03-16 08:34:48 +0000

    fro

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // ✅ IMPORTS CORRECTS
-import Login from './component/Sign up/Login';
 import Home from './component/home/Home';
 
 function App() {
   return (
     <div className="App">
-    
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
